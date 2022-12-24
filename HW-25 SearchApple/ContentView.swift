4a6bb6abc62b8338bce3572cfb51511213f50421
Ilya <iilavr@mail.ru>
2022-12-24 17:28:13 +0300

    Initial Commit

---
+++ HW-25 SearchApple/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW-25 SearchApple
+//
+//  Created by Илья on 24.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW-25 SearchApple/HW_25_SearchAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_25_SearchAppleApp.swift
+//  HW-25 SearchApple
+//
+//  Created by Илья on 24.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW_25_SearchAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
